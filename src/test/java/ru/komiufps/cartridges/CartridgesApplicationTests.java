a911f911b868b78766e69a692223fd1c7c7728ea
Denis.Zaboev <d.zaboev@ya.ru>
2021-04-07 20:06:42 +0300

    Initial commit

---
+++ src/test/java/ru/komiufps/cartridges/CartridgesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.komiufps.cartridges;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartridgesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
